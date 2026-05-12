90fcf72ab3bee9b2a778318c606ce556e50daf15
mineski-aj <andrewjames.s@met.events>
2026-05-12 13:14:22 +0800

    fixed gitignore

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL    = 'https://usyngchiqsobonfqskkm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzeW5nY2hpcXNvYm9uZnFza2ttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MTE4MDAsImV4cCI6MjA5MzE4NzgwMH0.gFFinSbb4Ye1966EcBxDu_FH9UBF-PC3XG4v_AJcg98';
+const IMPORT_PASSWORD = 'Mineski2019$@#';
